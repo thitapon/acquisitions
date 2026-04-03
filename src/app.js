d39fae21ed6ab86f1520142e2b070f9486c5f093
Thitapon Ousawat <thitapon@gmail.com>
2026-04-03 12:46:19 +0700

    updated .gitignore

--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 
+// add middlewares
+app.use(helmet());
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } })); 
+
+
+
 app.get('/', (req, res) => {
+  logger.info('Hello from Acquisitions!');
   res.status(200).send('Hello from Acquisitions!');
 });
 
