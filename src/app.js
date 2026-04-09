af31445727ec9de89d43677898a3c92c2de4126e
Thitapon Ousawat <thitapon@gmail.com>
2026-04-09 17:12:24 +0700

    adding authentication

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
+import authRoutes from '#routes/auth.routes.js';
+
 const app = express();
 
 // add middlewares
@@ -25,5 +27,15 @@ app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');
   res.status(200).send('Hello from Acquisitions!');
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK', timestamp: new Date().toISOString(), uptime: process.uptime() });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquisitions API is running!' });
+});
+
+app.use('/api/auth', authRoutes);
 
 export default app;
